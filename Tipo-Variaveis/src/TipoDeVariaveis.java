141dc241ab7e05d707672b51a5bad25365af6f62
Julio <silva.julio@aluno.ufabc.edu.br>
2024-05-08 17:37:01 -0300

    Anotações sobre os tipos de variáveis

---
+++ Tipo-Variaveis/src/TipoDeVariaveis.java
@@ -0,0 +1,22 @@
+public class TipoDeVariaveis {
+
+    /*
+    Estrutura padrão a ser seguida:
+    
+    <Tipo> <nomeVariavel> <atribuiçãoDeValor(Opcional)>
+
+    EX: int idade; <- Tipo "int", nome "idade" , sem nenhum valor atribuido
+        int ano = 2024 <- Tipo int, nome "ano", Valor 2024 
+        double reais = 2.5
+        String nome = "Julio"
+    ----------------------------------------------------------------------------
+
+    Já as constantes são valores que não podem ser modificados. São representados 
+    pela palavra reservada "final". Pòr convenção,as constantes são escritas em 
+    CAIXA ALTA
+    
+    EX: final double VALOR_DE_PI = 3.14;
+    final String NOME = "Julio"
+    */
+    
+}
